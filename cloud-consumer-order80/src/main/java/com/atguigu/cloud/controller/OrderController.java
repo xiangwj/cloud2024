b097761452d04051b8cee47c0b399de923a25808
wenjunxp@sina.com <wenjunxp@sina.com>
2025-03-03 21:10:50 +0800

    把cloud-consumer-order和cloud-payment-service注册到了consul上

--- cloud-consumer-order80/src/main/java/com/atguigu/cloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/cloud/controller/OrderController.java
@@ -16,7 +16,7 @@ import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class OrderController {
-    public static final String PaymentSrv_URL = "http://localhost:8001";
+    public static final String PaymentSrv_URL = "http://cloud-payment-service";
     @Resource
     private RestTemplate restTemplate;
 
