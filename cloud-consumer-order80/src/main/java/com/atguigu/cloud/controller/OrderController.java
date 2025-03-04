179061f697eff0df71cde986cff27a8c01894469
wenjunxp@sina.com <wenjunxp@sina.com>
2025-03-04 13:36:09 +0800

    写一个发现服务的代码

--- cloud-consumer-order80/src/main/java/com/atguigu/cloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/cloud/controller/OrderController.java
@@ -5,6 +5,9 @@ import com.atguigu.cloud.resp.ResultData;
 import com.atguigu.cloud.resp.ReturnCodeEnum;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -14,6 +17,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
+
+@Slf4j
 @RestController
 public class OrderController {
     public static final String PaymentSrv_URL = "http://cloud-payment-service";
@@ -45,21 +51,41 @@ public class OrderController {
 
     @GetMapping("/consumer/pay/list")
     public ResultData getListPayInfo() {
-        return restTemplate.getForObject(PaymentSrv_URL + "/pay/list" , ResultData.class);
+        return restTemplate.getForObject(PaymentSrv_URL + "/pay/list", ResultData.class);
     }
+
     @GetMapping("/consumer/error")
-    public ResultData<Integer> getPayError(){
+    public ResultData<Integer> getPayError() {
         Integer integer = Integer.valueOf("200");
-        try{
-            int age =10/0;
-        }catch (Exception e){
+        try {
+            int age = 10 / 0;
+        } catch (Exception e) {
             e.printStackTrace();
-            return ResultData.fail(ReturnCodeEnum.RC500.getCode(),e.getMessage());
+            return ResultData.fail(ReturnCodeEnum.RC500.getCode(), e.getMessage());
         }
         return ResultData.success(integer);
     }
+
     @GetMapping("/consumer/pay/info")
-    public String getInfoByConsul(){
-        return restTemplate.getForObject(PaymentSrv_URL+"/pay/get/info",String.class);
+    public String getInfoByConsul() {
+        return restTemplate.getForObject(PaymentSrv_URL + "/pay/get/info", String.class);
+    }
+
+    @Resource
+    private DiscoveryClient discoveryClient;
+
+    @GetMapping("/consumer/dicovery")
+    public String discovery() {
+        List<String> services = discoveryClient.getServices();
+        services.stream().forEach(System.out::println);
+        System.out.println("===================================");
+
+        List<ServiceInstance> instances = discoveryClient.getInstances("cloud-payment-service");
+
+        instances.stream().forEach(instance -> {
+            System.out.println(instance.getServiceId() + "\t" + instance.getHost() + "\t" + instance.getPort());
+        });
+        return instances.get(0).getServiceId() + "\t" + instances.get(0).getHost() + "\t" + instances.get(0).getPort();
+
     }
 }
