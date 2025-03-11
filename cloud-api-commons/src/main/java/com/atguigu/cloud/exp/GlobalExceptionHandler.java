def7988b0befd440c79d4465c67f597484d10a2b
wenjunxp@sina.com <wenjunxp@sina.com>
2025-03-11 22:55:48 +0800

    2025-03-11

--- cloud-api-commons/src/main/java/com/atguigu/cloud/exp/GlobalExceptionHandler.java
+++ cloud-api-commons/src/main/java/com/atguigu/cloud/exp/GlobalExceptionHandler.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
-@RestControllerAdvice
+//@RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(value = RuntimeException.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
