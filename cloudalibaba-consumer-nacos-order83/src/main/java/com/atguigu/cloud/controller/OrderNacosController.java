37c4a895f7f2e786998de1039a274fecedf9ac2f
wenjunxp@sina.com <wenjunxp@sina.com>
2025-03-12 16:37:58 +0800

    2025-03-12

--- cloudalibaba-consumer-nacos-order83/src/main/java/com/atguigu/cloud/controller/OrderNacosController.java
+++ cloudalibaba-consumer-nacos-order83/src/main/java/com/atguigu/cloud/controller/OrderNacosController.java
@@ -1,5 +1,7 @@
 package com.atguigu.cloud.controller;
 
+import com.atguigu.cloud.apis.PayFeignSentinelApi;
+import com.atguigu.cloud.resp.ResultData;
 import jakarta.annotation.Resource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,10 +15,16 @@ public class OrderNacosController {
     private RestTemplate restTemplate;
     @Value("${service-url.nacos-user-service}")
     private String serviceUrl;
-
+    @Resource
+    private PayFeignSentinelApi payFeignSentinelApi;
     @GetMapping("consumer/pay/nacos/{id}")
     public String paymentInfo(@PathVariable("id") Integer id) {
         String result = restTemplate.getForObject(serviceUrl + "/pay/nacos/" + id, String.class);
         return result+"\t"+"    我是OrderNacosController83调用者。。。。。。";
+    }
+    @GetMapping(value = "/consumer/pay/nacos/get/{orderNo}")
+    public ResultData getPayByOrderNo(@PathVariable("orderNo") String orderNo)
+    {
+        return payFeignSentinelApi.getPayByOrderNo(orderNo);
     }
 }
